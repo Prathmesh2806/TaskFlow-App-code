9879ea6f5ae31a1086878fdd4fe57c3bfa87978a
root <root@DESKTOP-7HI41TQ.localdomain>
2026-04-02 06:02:05 +0000

    made changes

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -23,24 +23,21 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = "${var.project_name}-${var.environment}-cluster"
-}
-
+# Use dynamic cluster auth - this works even during creation because it depends on the EKS module
 data "aws_eks_cluster_auth" "cluster" {
-  name = "${var.project_name}-${var.environment}-cluster"
+  name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
